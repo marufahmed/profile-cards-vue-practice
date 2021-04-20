27f6d80eb01094d2c7f742b41326792c301aba5a
Matthew Chertudi <matthew@metageek.net>
2021-04-20 12:25:52 -0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div v-for="profile in employees" :key="profile.id">
+     <profile-card :profile="profile" />
+   </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfileCard from './components/ProfileCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProfileCard,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          firstName: 'Lael',
+          lastName: 'Uberuaga-Rodgers',
+          email: 'lael@example.com',
+          phone: '(208)921-0001',
+          photo: 'https://prd-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/placeholder-profile_1.png'
+        },
+        {
+          id: 2,
+          firstName: 'Matthew',
+          lastName: 'Chertudi',
+          email: 'matthew@example.com',
+          phone: '(208)921-0002',
+          photo: 'https://prd-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/placeholder-profile_1.png'
+        },
+        {
+          id: 3,
+          firstName: 'Nabil',
+          lastName: 'Rahman',
+          email: 'nabil@example.com',
+          phone: '(208)921-0003',
+          photo: 'https://prd-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/placeholder-profile_1.png'
+        },
+        { 
+          id: 4,
+          firstName: 'Ryan',
+          lastName: 'Woodings',
+          email: 'ryan@example.com',
+          phone: '(208)921-0004',
+          photo: 'https://prd-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/placeholder-profile_1.png'
+        },
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    profile: {
+      type: Object, 
+      required: true,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
